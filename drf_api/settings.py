bd1b6b862e818ee3cfcf017594382f6b41355e8b
AsiaWi <joannawitek@icloud.com>
2023-11-27 22:47:40 +0000

    debug false

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -61,8 +61,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEV' in os.environ
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = ['8000-asiawi-djangorestfrmwrk-tp19mhm9en5.ws-eu106.gitpod.io', 'django-rest-frmwrk-9285b10668e7.herokuapp.com']
 
